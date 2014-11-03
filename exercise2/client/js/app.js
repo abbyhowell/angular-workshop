164eceb8e7951eff5e215c60e4d40cc05853c54f
abbyhowell <abby.howell@cengage.com>
2014-11-03 16:18:32 -0500

    exercise 3 completed - displaying categories in the view from a category factory

--- exercise2/client/js/app.js
+++ exercise2/client/js/app.js
@@ -1,6 +1,7 @@
 // initialize the app
-angular.module('TaskManager', []).run(function(TaskFactory) {
+angular.module('TaskManager', []).run(function(TaskFactory, CategoryFactory) {
     TaskFactory.fetch();
+    CategoryFactory.fetch();
 });
 
 angular.module('TaskManager').constant('ServerUrl', 'http://localhost:3000/');
@@ -23,14 +24,29 @@ angular.module('TaskManager').factory('TaskFactory', function($http, ServerUrl) 
     };
 });
 
-angular.module('TaskManager').controller('FormCtrl', function($scope, $http, ServerUrl, TaskFactory, $q) {
+angular.module('TaskManager').factory('CategoryFactory', function($http, ServerUrl) {
+    var categories = [];
+
+    var fetch = function() {
+        $http.get(ServerUrl + 'categories').success(function(response) {
+            // use angular.copy() to retain the original array which the controllers are bound to
+            // categories = response will overwrite the array with a new one and the controllers loose the reference
+            // could also do categories.length = 0, then push in the new items
+            angular.copy(response, categories);
+        });
+    };
+
+    return {
+        categories: categories,
+        fetch: fetch
+    };
+});
+
+angular.module('TaskManager').controller('FormCtrl', function($scope, $http, ServerUrl, TaskFactory, CategoryFactory, $q) {
     'use strict';
 
-    $http.get(ServerUrl + 'categories').success(function(response) {
-        $scope.categories = response;
-    });
-
     $scope.tasks = TaskFactory.tasks;
+    $scope.categories = CategoryFactory.categories;
 
     $scope.createCategory = function(category) {
         $scope.task = {};
@@ -49,8 +65,10 @@ angular.module('TaskManager').controller('FormCtrl', function($scope, $http, Ser
             $scope.tasks.push(response);
 
             $scope.task.name = '';
+
             $scope.task.category = '';
         });
+
     };
 
     $scope.removeCompleted = function() {
@@ -68,10 +86,11 @@ angular.module('TaskManager').controller('FormCtrl', function($scope, $http, Ser
     };
 });
 
-angular.module('TaskManager').controller('ListCtrl', function($scope, $http, ServerUrl, TaskFactory) {
+angular.module('TaskManager').controller('ListCtrl', function($scope, $http, ServerUrl, TaskFactory, CategoryFactory) {
     'use strict';
 
     $scope.tasks = TaskFactory.tasks;
+    $scope.categories = CategoryFactory.categories;
 
     $scope.toggleCompletion = function(task) {
         task.isCompleted = !task.isCompleted;
